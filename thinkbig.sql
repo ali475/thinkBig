cf443dd749022282cc9f3937dfd66f7a86a3d0fa
ali475 <32493519+ali475@users.noreply.github.com>
2018-10-27 00:57:46 +0200

    Add files via upload

---
+++ thinkbig.sql
@@ -0,0 +1,172 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 27, 2018 at 12:46 AM
+-- Server version: 10.1.32-MariaDB
+-- PHP Version: 7.2.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `thinkbig`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `descussion`
+--
+create database `thinkbig`
+CREATE TABLE `descussion` (
+  `id` int(11) NOT NULL,
+  `Text` text CHARACTER SET utf8 COLLATE utf8_bin,
+  `TopicID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `descussion`
+--
+
+INSERT INTO `descussion` (`id`, `Text`, `TopicID`) VALUES
+(1, 'المناقشه الاولي ', 1),
+(2, 'المناقشه 2', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hashtag`
+--
+
+CREATE TABLE `hashtag` (
+  `id` int(11) NOT NULL,
+  `Text` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `OPID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `opinion`
+--
+
+CREATE TABLE `opinion` (
+  `id` int(11) NOT NULL,
+  `Text` text CHARACTER SET utf8 COLLATE utf8_bin,
+  `Type` text,
+  `DescID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `topic`
+--
+
+CREATE TABLE `topic` (
+  `id` int(11) NOT NULL,
+  `Text` text CHARACTER SET utf8 COLLATE utf8_bin
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `topic`
+--
+
+INSERT INTO `topic` (`id`, `Text`) VALUES
+(1, 'هذا هو اول موضوع');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `descussion`
+--
+ALTER TABLE `descussion`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_n3y2mmtioef9gnwkb350dl21r` (`TopicID`);
+
+--
+-- Indexes for table `hashtag`
+--
+ALTER TABLE `hashtag`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_m0og15x0fqwj2cfvw8x9b2d1k` (`OPID`);
+
+--
+-- Indexes for table `opinion`
+--
+ALTER TABLE `opinion`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_fdtylt9eorh2btohntgf8c8fb` (`DescID`);
+
+--
+-- Indexes for table `topic`
+--
+ALTER TABLE `topic`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `descussion`
+--
+ALTER TABLE `descussion`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `hashtag`
+--
+ALTER TABLE `hashtag`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=252;
+
+--
+-- AUTO_INCREMENT for table `opinion`
+--
+ALTER TABLE `opinion`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `topic`
+--
+ALTER TABLE `topic`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `descussion`
+--
+ALTER TABLE `descussion`
+  ADD CONSTRAINT `FK_n3y2mmtioef9gnwkb350dl21r` FOREIGN KEY (`TopicID`) REFERENCES `topic` (`id`);
+
+--
+-- Constraints for table `hashtag`
+--
+ALTER TABLE `hashtag`
+  ADD CONSTRAINT `FK_m0og15x0fqwj2cfvw8x9b2d1k` FOREIGN KEY (`OPID`) REFERENCES `opinion` (`id`);
+
+--
+-- Constraints for table `opinion`
+--
+ALTER TABLE `opinion`
+  ADD CONSTRAINT `FK_fdtylt9eorh2btohntgf8c8fb` FOREIGN KEY (`DescID`) REFERENCES `descussion` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
